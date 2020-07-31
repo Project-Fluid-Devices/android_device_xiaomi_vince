aec7f53c4c3f117a4e8f917dfefb3b44897dbae2
sinchandasgupta <sinchandsgpt9@gmail.com>
2020-07-31 06:18:23 +0000

    vince: Fluid-ify
    Signed-off-by: sinchandasgupta <sinchandsgpt9@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_vince.mk
+    $(LOCAL_DIR)/fluid_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_vince-user \
-    havoc_vince-userdebug \
-    havoc_vince-eng
+    fluid_vince-user \
+	fluid_vince-userdebug \
+    fluid_vince-eng
